7ef07e2d1b7c558d4047bfa48399d8e355d322ed
Periphereia <227154629+violeae@users.noreply.github.com>
2026-04-18 16:14:24 +0800

    Create hexo-md-img.py

---
+++ hexo-md-img.py
@@ -0,0 +1,16 @@
+import re
+
+def markdown_to_html_img(text):
+    # Regex pattern: ![alt text](url)
+    # Group 1 = alt text, Group 2 = image URL
+    pattern = r'!\[(.*?)\]\((.*?)\)'
+    
+    # HTML replacement with no-referrer policy
+    replacement = r'<img src="\2" alt="\1" referrerpolicy="no-referrer">'
+    
+    return re.sub(pattern, replacement, text)
+
+markdown_data = input()
+html_result = markdown_to_html_img(markdown_data)
+
+print(html_result)
